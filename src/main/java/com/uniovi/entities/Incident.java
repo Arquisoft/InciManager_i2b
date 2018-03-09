ae8f05f2b53c6577cb90feb4c0677d3bffa4bceb
carmee-en <carmensors@gmail.com>
2018-03-10 00:42:03 +0100

    Added Jasypt password encryptor plus getters and setters

--- src/main/java/com/uniovi/entities/Incident.java
+++ src/main/java/com/uniovi/entities/Incident.java
@@ -1,15 +1,18 @@
 package com.uniovi.entities;
+import com.uniovi.util.JasyptEncryptor;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Incident {
 	
-	@Id
+	@Id @GeneratedValue
 	private Long id;
 	
 	private String username, password;
@@ -17,12 +20,74 @@ public class Incident {
 	private List<String> tags= new ArrayList<String>();
 	private List<String> moreInfo= new ArrayList<String>();
 
-	
 	public Incident() {}
+	
+	/**
+	 * Minimum initialization of Incident object. None of the parameters can be empty or null
+	 * 
+	 * @param username - of the user that submitted the incident
+	 * @param passw - of the user that submitted the incident
+	 * @param name - of the incident, either descriptive or a code
+	 * @param location - of the incident 
+	 */
+	public Incident(String username, String passw, String name, String location)
+	{
+		if (username=="" || passw=="" ||name=="" || location=="")
+			throw new IllegalArgumentException("Incident fields cannot be empty");
+		
+		this.username = username;
+		this.password = encryptPass(passw);
+		this.inciName = name;
+		this.location = location;
+	}
 	
 	public void addMoreInfo(String property, String value)
 	{
 		moreInfo.add(property +"/"+value);
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = encryptPass(password);
+	}
+
+	public void setInciName(String inciName) {
+		this.inciName = inciName;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getInciName() {
+		return inciName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public List<String> getMoreInfo() {
+		return moreInfo;
 	}
 
 	@Override
@@ -32,5 +97,10 @@ public class Incident {
 				.append(", inciName=").append(inciName).append(", location=").append(location).append(", tags=")
 				.append(tags).append(", moreInfo=").append(moreInfo).append("]");
 		return builder.toString();
+	}
+	
+	private String encryptPass(String password){
+		JasyptEncryptor encryptor = new JasyptEncryptor();
+		return encryptor.encryptPassword(password);
 	}
 }
