64c5cf0be1ee43d8713f730af46221b507b98415
carmee-en <carmensors@gmail.com>
2018-03-10 01:07:24 +0100

    Added tests for Incident class

--- src/test/java/com/uniovi/entities/IncidentTest.java
+++ src/test/java/com/uniovi/entities/IncidentTest.java
@@ -2,13 +2,53 @@ package com.uniovi.entities;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IncidentTest {
 
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCorrectInstatiation() {
+		Incident inci = new Incident("cArmeEn","2018#","BrokenFountain-10MAR","Plaza Nautico, Gijón");
+		assertFalse(inci.getPassword().equals("2018#"));
+		assertTrue(inci.getMoreInfo().isEmpty());
+		assertTrue(inci.getTags().isEmpty());
+	}
+	
+	@Test
+	public void testEmptyUsername() {
+		String username = "car";
+		String passw = "2018";
+		String name = "nameIncident";
+		String locat = "gijon";
+		
+		exception.expect(IllegalArgumentException.class);
+		Incident inci = new Incident("",passw, name, locat);
+
+		exception.expect(IllegalArgumentException.class);
+		inci = new Incident(username, "", name, locat);
+		
+		exception.expect(IllegalArgumentException.class);
+		inci = new Incident(username, passw, "", locat);
+		
+		exception.expect(IllegalArgumentException.class);
+		inci = new Incident(username, passw, name, "");
+	}
+	
+	@Test
+	public void testMoreInfo() {
+		Incident inci = new Incident("cArmeEn","2018#","BrokenFountain-10MAR","Plaza Nautico, Gijón");
+		inci.addMoreInfo("image", "BrokenFountain-10MAR.png");
+		inci.addMoreInfo("description", "Leaks at the base");
+		
+		assertEquals(2, inci.getMoreInfo().size());
+		assertEquals("image/BrokenFountain-10MAR.png", inci.getMoreInfo().get(0));
+		assertEquals("description/Leaks at the base", inci.getMoreInfo().get(1));
+
 	}
 
 }
