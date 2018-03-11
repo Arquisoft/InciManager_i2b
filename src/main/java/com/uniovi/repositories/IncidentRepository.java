4c7d20a61c554673dc9904204aee4a0db0156c30
ulitol97 <UO251436@uniovi.es>
2018-03-11 10:59:21 +0100

    Prepared IncidentRepository with findAll methods.

--- src/main/java/com/uniovi/repositories/IncidentRepository.java
+++ src/main/java/com/uniovi/repositories/IncidentRepository.java
@@ -1,8 +1,16 @@
 package com.uniovi.repositories;
 
-import com.uniovi.entities.Incident;
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface IncidentRepository extends CrudRepository<Incident, Long>{
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uniovi.entities.Incident;
+
+@Repository
+public interface IncidentRepository extends MongoRepository<Incident, Long>{
+	
+	List<Incident> findAllByUsername (String username);
+	List<Incident> findAllByUsernameAndPassword (String username);
 
 }
