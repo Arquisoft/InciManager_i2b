4dd5d82a755bc9abae0e1061a6811bc3416486b3
tejido18 <uo250821@uniovi.es>
2018-03-11 13:27:35 +0100

    CSVFileParser is added, and also its related tests

---
+++ src/main/java/com/uniovi/repositories/CSVFileParser.java
@@ -0,0 +1,80 @@
+package com.uniovi.repositories;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Repository;
+
+/**
+ * This class is in charge of parsing the master csv file
+ * that contains the mapping between the different agent codes
+ * and their name. It provides some methods to obtain the kind
+ * name from an agent given the kind code, and also to obtain
+ * the kind code given the kind name.
+ * 
+ * @author Alejandro González Hevia
+ *
+ */
+@Repository
+public class CSVFileParser implements MasterFileParser {
+	
+	private String filePath;
+	
+	public CSVFileParser() {
+		this.filePath = "master.csv";
+	}
+	
+	public CSVFileParser(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getKindNameOf(int kind) throws FileNotFoundException, IOException {
+		Reader reader = null;
+		
+        try {
+	        	reader = new FileReader(filePath);
+	        	Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(reader);
+	        	for (CSVRecord record : records) {
+	        		this.assertValidRecord(record);
+	        		
+	        	    int kindCode = Integer.valueOf(record.get(0));
+	        	    if (kindCode == kind) return record.get(1);
+	        	}
+        } finally {
+            if (reader != null) { try { reader.close(); } catch (IOException e) { e.printStackTrace(); } }
+        }
+        
+		return null;
+	}
+
+	public int getKindCodeOf(String kindName) throws FileNotFoundException, IOException {
+		if (kindName == null) return -1;
+		
+		Reader reader = null;
+		
+        try {
+	        	reader = new FileReader(filePath);
+	        	Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(reader);
+	        	for (CSVRecord record : records) {
+	        		this.assertValidRecord(record);
+	        		
+	        	    String name = record.get(1);
+	        	    if (kindName.equals(name)) return Integer.valueOf(record.get(0));
+	        	}
+        } finally {
+            if (reader != null) { try { reader.close(); } catch (IOException e) { e.printStackTrace(); } }
+        }
+        
+		return -1;
+	}
+
+	
+	private void assertValidRecord(CSVRecord record) {
+		assert (record.size() == 2) : "Invalid number of columns";
+	}
+
+}
---
+++ src/test/java/com/uniovi/main/repositories/CSVParserTest.java
@@ -0,0 +1,72 @@
+package com.uniovi.main.repositories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.uniovi.repositories.CSVFileParser;
+
+public class CSVParserTest {
+	
+	private CSVFileParser parser;
+	
+	@Test
+	public void getKindNameTest() throws FileNotFoundException, IOException {
+		parser = new CSVFileParser("src/test/master_test.csv");
+		assertEquals("Person", parser.getKindNameOf(1));
+		assertEquals("Entity", parser.getKindNameOf(2));
+		assertEquals("Sensor", parser.getKindNameOf(3));
+		assertEquals(null, parser.getKindNameOf(0));
+		assertEquals(null, parser.getKindNameOf(4));
+	}
+	
+	@Test
+	public void getKindCodeTest() throws FileNotFoundException, IOException {
+		parser = new CSVFileParser("src/test/master_test.csv");
+		assertEquals(1, parser.getKindCodeOf("Person"));
+		assertEquals(2, parser.getKindCodeOf("Entity"));
+		assertEquals(3, parser.getKindCodeOf("Sensor"));
+		assertEquals(-1, parser.getKindCodeOf("Son"));
+		assertEquals(-1, parser.getKindCodeOf(null));
+	}
+	
+	@Test
+	public void fileNotFoundTest() {
+		parser = new CSVFileParser("src/test/not_a_file.csv");
+		try {
+			parser.getKindNameOf(1);
+			fail();
+		} catch (FileNotFoundException fnfe) {
+			// should throw this
+		} catch (IOException e) {
+			fail();
+		}
+		
+		try {
+			parser.getKindCodeOf("Person");
+			fail();
+		} catch (FileNotFoundException fnfe) {
+			// should throw this
+		} catch (IOException e) {
+			fail();
+		}
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void wrongFormatTest() {
+		parser = new CSVFileParser("src/test/wrongFile.csv");
+		
+			try {
+				parser.getKindNameOf(1);
+			} catch (IOException e) {
+				fail();
+			}
+			fail();
+		 
+	}
+
+}
